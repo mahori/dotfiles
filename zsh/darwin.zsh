c372c87051215f35ffd9b52e3175a51bc84b9b91
mahori <mahori@users.noreply.github.com>
2018-01-31 08:42:09 +0900

    CPANからインストールしたPerlスクリプトを実行可能にした

--- zsh/darwin.zsh
+++ zsh/darwin.zsh
@@ -4,7 +4,7 @@ export PERL5LIB="${HOME}/local/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"
 export PERL_LOCAL_LIB_ROOT="${HOME}/local/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_ROOT}"
 export PERL_MB_OPT="--install_base \"${HOME}/local/perl5\""
 export PERL_MM_OPT="INSTALL_BASE=${HOME}/local/perl5"
-export PATH="${HOME}/bin:/usr/local/opt/qt/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/libexec:/opt/X11/bin"
+export PATH="${HOME}/bin:${HOME}/local/perl5/bin:/usr/local/opt/qt/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/libexec:/opt/X11/bin"
 
 case ${UID} in
     501)
