318abb9f44bd9d66128b83936ea8a473a79c41cc
mahori <mahori@users.noreply.github.com>
2018-09-20 12:58:58 +0900

    wgrep-agパッケージを削除

--- emacs/inits/30-ag.el
+++ emacs/inits/30-ag.el
@@ -4,15 +4,3 @@
   (setq ag-highlight-search t)
   (setq ag-reuse-window t)
   )
-
-(use-package wgrep-ag
-  :ensure t
-  :config
-  (defun mahori:ag-mode-hook ()
-    (setq wgrep-auto-save-buffer t)
-    (setq wgrep-enable-key "r")
-    (wgrep-ag-setup)
-    )
-
-  (add-hook 'ag-mode-hook 'mahori:ag-mode-hook)
-  )
