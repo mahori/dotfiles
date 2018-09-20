98c614861ccfde7d448331009657b0f7d289c3d4
mahori <mahori@users.noreply.github.com>
2018-09-20 12:51:33 +0900

    関数名を統一

--- emacs/inits/50-c++-mode.el
+++ emacs/inits/50-c++-mode.el
@@ -1,5 +1,5 @@
-(defun mahori:c++-indent-hook ()
+(defun mahori:c++-mode-indent-hook ()
   (c-set-offset 'innamespace 0)
   )
 
-(add-hook 'c++-mode-hook 'mahori:c++-indent-hook)
+(add-hook 'c++-mode-hook 'mahori:c++-mode-indent-hook)
--- emacs/inits/50-dns-mode.el
+++ emacs/inits/50-dns-mode.el
@@ -1,6 +1,6 @@
-(defun mahori:dns-tab-hook ()
+(defun mahori:dns-mode-indent-hook ()
   (setq indent-tabs-mode t)
   (setq tab-width 8)
   )
 
-(add-hook 'dns-mode-hook 'mahori:dns-tab-hook)
+(add-hook 'dns-mode-hook 'mahori:dns-mode-indent-hook)
