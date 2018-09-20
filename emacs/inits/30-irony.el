0af3a10938ec73d0a42a5da72f97d3d7566b91ce
mahori <mahori@users.noreply.github.com>
2018-09-20 10:38:51 +0900

    ironyパッケージを削除
    30-irony.elが不要になったので削除

--- emacs/inits/30-irony.el
+++
@@ -1,8 +0,0 @@
-(use-package irony
-  :ensure t
-  :hook ((c-mode . irony-mode)
-         (c++-mode . irony-mode)
-         (objc-mode . irony-mode))
-  :config
-  (setq irony--server-executable (expand-file-name "irony/bin/irony-server" user-emacs-directory))
-  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
