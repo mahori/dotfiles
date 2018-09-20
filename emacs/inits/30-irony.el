d639ab0caefc4e475f52e0a812092fb2d79d9b10
mahori <mahori@users.noreply.github.com>
2018-09-20 10:36:48 +0900

    company-ironyパッケージを削除

--- emacs/inits/30-irony.el
+++ emacs/inits/30-irony.el
@@ -6,8 +6,3 @@
   :config
   (setq irony--server-executable (expand-file-name "irony/bin/irony-server" user-emacs-directory))
   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
-
-(use-package company-irony
-  :ensure t
-  :config
-  (add-to-list 'company-backends 'company-irony))
