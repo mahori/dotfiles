8a26810f235ac9feab76a30b88ab5838997adc0e
mahori <mahori@users.noreply.github.com>
2018-09-20 13:50:02 +0900

    hookの書き方を調整

--- emacs/inits/30-ggtags.el
+++ emacs/inits/30-ggtags.el
@@ -1,11 +1,10 @@
+(defun mahori:ggtags-hook ()
+  (ggtags-mode 1)
+  (setq-local hippie-expand-try-functions-list
+              (cons 'ggtags-try-complete-tag hippie-expand-try-functions-list)))
+
 (use-package ggtags
   :ensure t
-  :config
-  (defun mahori:ggtags-hook ()
-    (ggtags-mode 1)
-    (setq-local hippie-expand-try-functions-list
-                (cons 'ggtags-try-complete-tag hippie-expand-try-functions-list)))
-
-  (add-hook 'c-mode-common-hook 'mahori:ggtags-hook)
-  (add-hook 'cperl-mode-hook 'mahori:ggtags-hook)
+  :commands (ggtags-mode mahori:ggtags-hook)
+  :hook ((c-mode-common cperl-mode) . mahori:ggtags-hook)
   )
