963a78e959bc78105651b8738851132d8a1b1e71
mahori <mahori@users.noreply.github.com>
2019-04-07 15:21:16 +0900

    記述方法を統一 (#21)

--- zsh/kc.zsh
+++ zsh/kc.zsh
@@ -1,11 +1,11 @@
 kc ()
 {
-    local key_file=${HOME}/.ssh/id_rsa
-    local keychain_file=${HOME}/.keychain/${HOST}-sh
+    local key_file="${HOME}/.ssh/id_rsa"
+    local keychain_file="${HOME}/.keychain/${HOST}-sh"
 
     pgrep -q ssh-agent || keychain --clear && keychain ${key_file}
 
-    if [ -f ${keychain_file} ]
+    if [[ -f ${keychain_file} ]]
     then
        source ${keychain_file}
     fi
