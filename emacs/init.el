da5b9ddef9c19978dce3bb136418286cea535c9b
mahori <mahori@users.noreply.github.com>
2019-05-14 10:11:35 +0900

    which-key-posframeパッケージを導入 (#25)

--- emacs/init.el
+++ emacs/init.el
@@ -252,6 +252,14 @@
     :hook ((cperl-mode emacs-lisp-mode) . company-posframe-mode)
     )
 
+  (use-package which-key-posframe
+    :ensure t
+    :after (posframe which-key)
+    :config
+    (which-key-posframe-mode 1)
+    (setq which-key-posframe-poshandler 'posframe-poshandler-frame-center)
+    )
+
   (use-package yasnippet
     :ensure t
     :hook ((cperl-mode emacs-lisp-mode sh-mode) . yas-minor-mode)
