0b6970ea986ade1e6c909534df52d13004a723b8
HASHIMOTO Masayuki <gh@mahori.mail-box.ne.jp>
2020-01-09 17:34:10 +0900

    CPANの設定を追加 (#52)

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -1,4 +1,8 @@
 export LD_LIBRARY_PATH="/usr/lib/oracle/19.5/client64/lib:/usr/local/lib${LD_LIBRARY_PATH+:}${LD_LIBRARY_PATH}"
+export PERL5LIB="${HOME}/local/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"
+export PERL_LOCAL_LIB_ROOT="${HOME}/local/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_ROOT}"
+export PERL_MB_OPT="--install_base \"${HOME}/local/perl5\""
+export PERL_MM_OPT="INSTALL_BASE=${HOME}/local/perl5"
 
 case ${UID} in
     0)
