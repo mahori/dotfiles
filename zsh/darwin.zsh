fe15a99be531f46233fb48d64dd8ad5d28f64f00
HASHIMOTO Masayuki <gh@mahori.mail-box.ne.jp>
2020-01-28 20:53:32 +0900

    Oracleデータベース接続の設定を追加 (#58)

--- zsh/darwin.zsh
+++ zsh/darwin.zsh
@@ -4,7 +4,9 @@ export PERL5LIB="${HOME}/local/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"
 export PERL_LOCAL_LIB_ROOT="${HOME}/local/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_ROOT}"
 export PERL_MB_OPT="--install_base \"${HOME}/local/perl5\""
 export PERL_MM_OPT="INSTALL_BASE=${HOME}/local/perl5"
-export PATH="${HOME}/bin:${HOME}/local/perl5/bin:/usr/local/opt/qt/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/libexec:/opt/X11/bin"
+export ORACLE_HOME="${HOME}/local/oracle"
+export DYLD_LIBRARY_PATH="${ORACLE_HOME}${DYLD_LIBRARY_PATH+:}${DYLD_LIBRARY_PATH}"
+export PATH="${HOME}/bin:${HOME}/local/perl5/bin:${ORACLE_HOME}:/usr/local/opt/qt/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/libexec:/opt/X11/bin"
 
 case ${UID} in
     501)
