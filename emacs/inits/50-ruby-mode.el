875dbb89396641f211e3a0a0e8e41b4255f3c6b3
HASHIMOTO Masayuki <gh@mahori.mail-box.ne.jp>
2020-02-06 19:43:35 +0900

    ruby-mode時の設定を追加 (#60)

---
+++ emacs/inits/50-ruby-mode.el
@@ -0,0 +1,10 @@
+(defun mahori:ruby-mode-hook ()
+  (auto-revert-mode 1)
+  (company-mode 1)
+  (display-line-numbers-mode 1)
+  (hl-line-mode 1)
+  (show-paren-mode 1)
+  (yas-minor-mode 1)
+  )
+
+(add-hook 'ruby-mode-hook 'mahori:ruby-mode-hook)
