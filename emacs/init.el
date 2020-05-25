34d7f84367c68471550c6d37dc0dfafa68e512df
mahori <mahori@users.noreply.github.com>
2020-05-25 15:42:15 +0900

    初期ウィンドウサイズを調整

--- emacs/init.el
+++ emacs/init.el
@@ -172,7 +172,8 @@
 
 (when (and (eq system-type 'darwin)
            (>= (user-uid) 1000))
-  (setq default-frame-alist '((width . 130) (height . 55)))
+  (setq default-frame-alist '((width  . 200)
+                              (height . 75)))
 
   (set-face-attribute 'default nil :family "Ricty Discord" :height 150)
 
