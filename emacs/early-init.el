5f1a93737e1b1f7a01eca79d3080e2376ef49672
mahori <mahori@users.noreply.github.com>
2020-10-09 15:26:49 +0900

    画面表示周りを調整

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,9 +1,9 @@
 (when (and (eq system-type 'darwin)
            (>= (user-uid) 1000))
-  (setq default-frame-alist '((width  . 200)
-                              (height . 75)))
+  (setq default-frame-alist '((width  . 128)
+                              (height . 64)))
 
-  (set-face-attribute 'default nil :family "Ricty Discord" :height 150)
+  (set-face-attribute 'default nil :family "HackGen" :height 150)
 
   (scroll-bar-mode 0)
   (tool-bar-mode 0)
